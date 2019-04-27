56fd0c033bb9c477cdef7152662b17be0c8c3a1a
Jim Hough <teachlife@outlook.com>
2019-04-27 01:25:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,5 +74,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holidays = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, items|
+      if items.include? ('BBQ')
+        holidays << holiday
+      end
+    end
+  end
+  holidays
+end
+  
 end
